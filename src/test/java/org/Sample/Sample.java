ad6acc8700cf3fa5bd4bc98f2d2fab2e7d816b77
Krsh2002 <krishna.college1912@gmail.com>
2024-09-12 22:04:35 +0530

    Updated code

--- src/test/java/org/Sample/Sample.java
+++ src/test/java/org/Sample/Sample.java
@@ -3,6 +3,9 @@ package org.Sample;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Git");
+		System.out.println("Git4");
+		System.out.println("Git5");
+		System.out.println("Git6");
 		// first sample 
 	}
 
